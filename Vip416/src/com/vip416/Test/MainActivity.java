15b23765f1d48875ec7a01560139886700c7042e
administration <tianyu96x@126.com>
2016-03-17 21:28:21 +0800

    121221

--- Vip416/src/com/vip416/Test/MainActivity.java
+++ Vip416/src/com/vip416/Test/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	 //----------------------------------------
+	 //----------------------------------------00000
 	}
 
 	@Override
